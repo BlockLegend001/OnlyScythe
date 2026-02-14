b81076ec27ca47b65002b96129268a45c31343d6
BlockLegend001 <rosarioarcamone@gmail.com>
2026-02-14 18:04:27 +0100

    Added Copper Scythe

--- common/src/main/java/com/blocklegend001/onlyscythe/item/ModItems.java
+++ common/src/main/java/com/blocklegend001/onlyscythe/item/ModItems.java
@@ -18,7 +18,7 @@ public class ModItems {
     public static final Item COPPER_SCYTHE = new Scythe(ToolMaterial.COPPER, -2.0F, -1.0F,  new Item.Properties()
             .setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "copper_scythe"))));
 
-    public static final Item GOLDEN_SCYTHE = new Scythe(ToolMaterial.GOLD, 0.0F, -3.0F,  new Item.Properties()
+    public static final Item GOLDEN_SCYTHE = new Scythe(ToolMaterial.GOLD, -1.0F, -3.0F,  new Item.Properties()
             .setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "golden_scythe"))));
 
     public static final Item IRON_SCYTHE = new Scythe(ToolMaterial.IRON, -2.0F, -1.0F,  new Item.Properties()
